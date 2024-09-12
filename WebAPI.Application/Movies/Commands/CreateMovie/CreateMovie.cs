4b85220b9c975d88001b9ca3b21baddc5b3a7321
Daniel105 <danielpinto105@gmail.com>
2024-09-12 15:14:38 +0100

    Updated

--- WebAPI.Application/Movies/Commands/CreateMovie/CreateMovie.cs
+++ WebAPI.Application/Movies/Commands/CreateMovie/CreateMovie.cs
@@ -12,17 +12,6 @@ public class CreateMovieCommandHandlerMovie(IRepository<Movie> movieRepository)
 
     public Movie CreateMovie(Movie movie)
     {
-        Movie harryPotter = new()
-        {
-            Title = "Harry Potter",
-            Description = $"Harry Potter and the Chamber of Secrets is a 2002 fantasy film directed by Chris Columbus " +
-                $"from a screenplay by Steve Kloves. " +
-                $"It is based on the 1998 novel Harry Potter and the Chamber of Secrets by J. K. Rowling.",
-            Author = "J. K. Rowling",
-            Duration = 120,
-            Url = "https://www.imdb.com/title/tt0295297/"
-        };
-      
         _movieRepository.Add(movie);
         return movie;
     }
--- WebAPI/Controllers/MovieController.cs
+++ WebAPI/Controllers/MovieController.cs
@@ -10,20 +10,29 @@ namespace WebAPI.Controllers
     {
 
         private readonly ILogger<MovieController> _logger;
-        private readonly CreateMovieCommandHandler _createMovieCommandHandler;
+        private readonly CreateMovieCommandHandlerMovie _createMovieCommandHandler;
 
-        public MovieController(ILogger<MovieController> logger, CreateMovieCommandHandler createMovieCommandHandler)
+        public MovieController(ILogger<MovieController> logger, CreateMovieCommandHandlerMovie createMovieCommandHandler)
         {
             _logger = logger;
             _createMovieCommandHandler = createMovieCommandHandler;
         }
 
         [HttpGet(Name = "GetMovie")]
-        public IEnumerable<Movie> Get()
+        public Movie Get()
         {
-            Movie myMovie = _createMovieCommandHandler.HandleSingleOperation();
-            IEnumerable<Movie> movies = new List<Movie>();
-            return movies;
+            Movie harryPotter = new()
+            {
+                Title = "Harry Potter",
+                Description = $"Harry Potter and the Chamber of Secrets is a 2002 fantasy film directed by Chris Columbus " +
+                $"from a screenplay by Steve Kloves. " +
+                $"It is based on the 1998 novel Harry Potter and the Chamber of Secrets by J. K. Rowling.",
+                Author = "J. K. Rowling",
+                Duration = 120,
+                Url = "https://www.imdb.com/title/tt0295297/"
+            };
+            Movie myMovie = _createMovieCommandHandler.CreateMovie(harryPotter);
+            return myMovie;
 
         }
     }
